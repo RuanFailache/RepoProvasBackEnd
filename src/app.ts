66609680f947198283e543068bf45e2c6c669269
RuanFailache <ruanfailache@outlook.com>
2021-12-17 16:59:11 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import 'reflect-metadata';
+import express from 'express'
+import cors from 'cors'
+import 'reflect-metadata'
 
-import connectDatabase from './database';
+import connectDatabase from './database'
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
 export async function init() {
-  await connectDatabase();
+  await connectDatabase()
 }
 
-export default app;
+export default app
--- src/setup.ts
+++ src/setup.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-const pathFile = process.env.NODE_ENV === 'production'
-  ? '.env'
-  : '.local.env'
-  ;
+const pathFile = process.env.NODE_ENV === 'production' ? '.env' : '.local.env'
 
 dotenv.config({
   path: pathFile,
-});
+})
